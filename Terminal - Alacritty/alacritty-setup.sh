beb77c6e790efe56879e2304bfb990be6402bd11
Ian McKay <tehmuffzor@gmail.com>
2021-01-18 15:49:09 -0600

    add tmux install and config

--- Terminal - Alacritty/alacritty-setup.sh
+++ Terminal - Alacritty/alacritty-setup.sh
@@ -2,10 +2,13 @@
 # git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core fetch --unshallow
 # git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask fetch --unshallow
 # brew install --cask alacritty
+# a multiplexer is necessary as Alacritty doesn't have GUI tabs
+# brew install tmux
 
-# Use existing config
+# Use existing configs
 mkdir -p "$HOME/.config/alacritty" 
 cp ./alacritty.yml "$HOME/.config/alacritty"
+cp ./.tmux.conf "$HOME/" 
 
 # Import shades of purple theme SEE:https://github.com/ahmadawais/shades-of-purple-alacritty/
 curl https://raw.githubusercontent.com/ahmadawais/shades-of-purple-alacritty/master/alacritty.yml >> $HOME/.config/alacritty/alacritty.yml
